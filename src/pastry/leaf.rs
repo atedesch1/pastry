f3aeee8bcba943de5a6a46a41f1117c5cd546b74
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-09-24 15:07:31 -0300

    comment and fix leaf_set

--- src/pastry/leaf.rs
+++ src/pastry/leaf.rs
@@ -1,6 +1,7 @@
 use crate::error::{Error, Result};
 use std::vec;
 
+/// A key-value pair data structure used in LeafSet
 #[derive(Debug, Clone, PartialEq)]
 pub struct KeyValuePair<T, U> {
     pub key: T,
@@ -13,6 +14,8 @@ impl<T, U> KeyValuePair<T, U> {
     }
 }
 
+/// LeafSet is a data structure used in the Pastry routing algorithm. The leaf set is a data
+/// structure that holds connections to the closests neighbors to a node.
 #[derive(Debug, Clone)]
 pub struct LeafSet<T: Clone> {
     max_size: usize,
@@ -21,7 +24,19 @@ pub struct LeafSet<T: Clone> {
 }
 
 impl<T: Clone> LeafSet<T> {
-    pub fn new(k: usize, initial_key: u64, initial_value: T) -> Result<Self> {
+    /// Creates a new instance of the LeafSet with the specified parameters.
+    ///
+    /// # Arguments
+    ///
+    /// * `k` - The size parameter `k` for the LeafSet. Must be greater than or equal to 1.
+    /// * `key` - The main key for the LeafSet.
+    /// * `value` - The main value for the LeafSet.
+    ///
+    /// # Returns
+    ///
+    /// A Result containing the newly created LeafSet if successful, or an Error if `k` is less than 1.
+    ///
+    pub fn new(k: usize, key: u64, value: T) -> Result<Self> {
         if k < 1 {
             return Err(Error::Config("cannot have leaf set with k < 1".into()));
         }
@@ -29,18 +44,43 @@ impl<T: Clone> LeafSet<T> {
         Ok(Self {
             max_size: 2 * k + 1,
             node_idx: 0,
-            set: vec![KeyValuePair::new(initial_key, initial_value)],
+            set: vec![KeyValuePair::new(key, value)],
         })
     }
 
+    /// Inserts a key-value pair into the LeafSet, maintaining its size and order.
+    ///
+    /// If the LeafSet is not full, the new pair is inserted into the appropriate position based on the key,
+    /// and the `node_idx` is updated if necessary.
+    ///
+    /// If the LeafSet is already full, the new pair replaces an existing pair based on the key's position.
+    /// The LeafSet is then sorted by key to maintain order.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to insert.
+    /// * `value` - The value associated with the key.
+    ///
+    /// # Returns
+    ///
+    /// An `Ok(())` result if the insertion was successful.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if:
+    ///
+    /// * Key is outside the set, indicating an internal error.
+    ///
     pub fn insert(&mut self, key: u64, value: T) -> Result<()> {
         let new_pair = KeyValuePair::new(key, value);
 
         if self.set.len() < self.max_size {
             let mut position = self.find_responsible(key).unwrap();
 
-            if position != self.set.len() && key > self.set[position].key {
+            if position < self.set.len() && key > self.set[position].key {
                 position += 1;
+            } else if position == self.set.len() - 1 && key < self.set[position].key {
+                position = 0;
             }
 
             self.set.insert(position, new_pair);
@@ -66,6 +106,26 @@ impl<T: Clone> LeafSet<T> {
         Ok(())
     }
 
+    /// Removes a key-value pair from the LeafSet based on the provided key.
+    ///
+    /// The key-value pair is removed from the set, and if the removed key is on the left side of the node index,
+    /// the `node_idx` is decremented accordingly.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The key to remove from the LeafSet.
+    ///
+    /// # Returns
+    ///
+    /// An `Ok(())` result if the removal was successful.
+    ///
+    /// # Errors
+    ///
+    /// An error is returned if:
+    ///
+    /// * The specified key cannot be found in the LeafSet, indicating an internal error.
+    /// * The specified key is in the same position as the `node_idx`, indicating it cannot be removed.
+    ///
     pub fn remove(&mut self, key: u64) -> Result<()> {
         let position = self
             .set
@@ -89,9 +149,47 @@ impl<T: Clone> LeafSet<T> {
         Ok(())
     }
 
+    /// Gets owner value of supplied key.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The main key for the LeafSet.
+    ///
+    /// # Returns
+    ///
+    /// An Option containing the owner value of the supplied key, or None if not found.
+    ///
     pub fn get(&self, key: u64) -> Option<T> {
         self.find_responsible(key)
             .map(|idx| self.set[idx].value.clone())
+    }
+
+    pub fn get_first_index(&self) -> Option<usize> {
+        if self.set.len() < self.max_size {
+            return None;
+        }
+
+        Some((self.max_size + self.node_idx - self.max_size / 2) % self.max_size)
+    }
+
+    pub fn get_last_index(&self) -> Option<usize> {
+        if self.set.len() < self.max_size {
+            return None;
+        }
+
+        Some((self.node_idx + self.max_size / 2) % self.max_size)
+    }
+
+    pub fn get_set(&self) -> Vec<KeyValuePair<u64, T>> {
+        self.set.clone()
+    }
+
+    pub fn get_node_index(&self) -> usize {
+        self.node_idx
+    }
+
+    pub fn is_full(&self) -> bool {
+        self.set.len() == self.max_size
     }
 
     fn find_responsible(&self, key: u64) -> Option<usize> {
@@ -119,22 +217,6 @@ impl<T: Clone> LeafSet<T> {
         Some(position)
     }
 
-    pub fn get_first_index(&self) -> Option<usize> {
-        if self.set.len() < self.max_size {
-            return None;
-        }
-
-        Some((self.max_size + self.node_idx - self.max_size / 2) % self.max_size)
-    }
-
-    pub fn get_last_index(&self) -> Option<usize> {
-        if self.set.len() < self.max_size {
-            return None;
-        }
-
-        Some((self.node_idx + self.max_size / 2) % self.max_size)
-    }
-
     fn is_right_neighbor(&self, idx: usize) -> Result<bool> {
         if idx >= self.set.len() {
             return Err(Error::Internal("index is out of bounds".into()));
@@ -151,18 +233,6 @@ impl<T: Clone> LeafSet<T> {
         } else {
             (self.node_idx <= idx && idx < self.set.len()) || (idx <= last_index)
         })
-    }
-
-    pub fn get_set(&self) -> Vec<KeyValuePair<u64, T>> {
-        self.set.clone()
-    }
-
-    pub fn get_node_index(&self) -> usize {
-        self.node_idx
-    }
-
-    pub fn is_full(&self) -> bool {
-        self.set.len() == self.max_size
     }
 }
 
