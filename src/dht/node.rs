72954ac4ad1d743e97346856bac6cca9e86fd974
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-09-24 15:13:47 -0300

    update proto and service

--- src/dht/node.rs
+++ src/dht/node.rs
@@ -12,7 +12,8 @@ use crate::{
     hring::hasher::Sha256Hasher,
     pastry::{leaf::LeafSet, table::RoutingTable},
     rpc::node::{
-        node_service_client::NodeServiceClient, node_service_server::NodeServiceServer, JoinRequest,
+        node_service_client::NodeServiceClient, node_service_server::NodeServiceServer,
+        JoinRequest, UpdateNeighborsRequest,
     },
     util::HasID,
 };
@@ -55,7 +56,7 @@ pub struct Node {
     pub leaf: Arc<RwLock<LeafSet<NodeConnection>>>,
     pub table: Arc<RwLock<RoutingTable<NodeInfo>>>,
 
-    pub is_connected: Arc<Mutex<bool>>,
+    is_connected: Arc<Mutex<()>>,
 }
 
 /// Configuration for the Pastry network
@@ -65,13 +66,24 @@ pub struct PastryConfig {
 }
 
 impl Node {
-    /// Creates a new DHT node which will be available publicly on
+    /// Registers a new DHT node which will be available publicly on
     /// http://hostname:port
+    ///
+    /// # Arguments
+    ///
+    /// * `config` - The Pastry network configuration.
+    /// * `hostname` - The Hostname to serve this node on.
+    /// * `port` - The port to serve this node on.
+    ///
+    /// # Returns
+    ///
+    /// A Result containing the newly registered node.
+    ///
     pub fn new(config: PastryConfig, hostname: &str, port: &str) -> Result<Self> {
         let pub_addr = format!("http://{}:{}", hostname, port);
         let addr = format!("0.0.0.0:{}", port);
-        let id = Sha256Hasher::hash_once(pub_addr.as_bytes());
 
+        let id = Sha256Hasher::hash_once(pub_addr.as_bytes());
         info!("Registering node as {} = #{:x}", id, id);
 
         Ok(Node {
@@ -84,12 +96,21 @@ impl Node {
                 NodeConnection::new(id, &pub_addr, None),
             )?)),
             table: Arc::new(RwLock::new(RoutingTable::new(id))),
-            is_connected: Arc::new(Mutex::new(false)),
+            is_connected: Arc::new(Mutex::new(())),
         })
     }
 
-    /// Serves server and connects to bootstrap node if exists
-    /// else creates own network.
+    /// Connects to network via bootstrap node and serves node server.
+    /// Consumes node.
+    ///
+    /// # Arguments
+    ///
+    /// * `bootstrap_addr` - A bootstrap node address.
+    ///
+    /// # Returns
+    ///
+    /// An empty Result.
+    ///
     pub async fn bootstrap_and_serve(self, bootstrap_addr: Option<&str>) -> Result<()> {
         info!("Initializing #{:x} on {}", self.id, self.pub_addr);
 
@@ -104,8 +125,7 @@ impl Node {
             ));
         } else {
             info!("Initializing network");
-            let mut guard = self.is_connected.lock().await;
-            *guard = true;
+            let _ = self.is_connected.lock().await;
             info!("Connected to network");
         }
 
@@ -116,40 +136,68 @@ impl Node {
             .await?)
     }
 
-    /// Connects to bootstrap node if exists and joins network, else
-    /// creates own network.
+    /// Connects to bootstrap node.
     async fn connect_to_network(
         id: u64,
         pub_addr: String,
         leaf: Arc<RwLock<LeafSet<NodeConnection>>>,
         table: Arc<RwLock<RoutingTable<NodeInfo>>>,
-        is_connected: Arc<Mutex<bool>>,
+        is_connected: Arc<Mutex<()>>,
         bootstrap_addr: String,
     ) -> Result<()> {
-        let mut guard = is_connected.lock().await;
+        let _ = is_connected.lock().await;
 
         info!("Connecting to network through #{:x}", id);
 
         let mut client = NodeServiceClient::connect(bootstrap_addr.to_owned()).await?;
         let join_response = client
-            .join(JoinRequest { id, pub_addr })
+            .join(JoinRequest {
+                id,
+                pub_addr: pub_addr.clone(),
+            })
             .await?
             .into_inner();
+        println!(
+            "Node #{} join_response: {:?}",
+            id,
+            join_response
+                .leaf_set
+                .iter()
+                .map(|e| e.id)
+                .collect::<Vec<u64>>()
+        );
 
         let mut leaf = leaf.write().await;
         for entry in join_response.leaf_set {
-            let client = NodeServiceClient::connect(entry.pub_addr.clone()).await?;
+            let mut client = NodeServiceClient::connect(entry.pub_addr.clone()).await?;
+            client
+                .update_neighbors(UpdateNeighborsRequest {
+                    id,
+                    pub_addr: pub_addr.clone(),
+                })
+                .await?;
             leaf.insert(
                 entry.id,
                 NodeConnection::new(entry.id, &entry.pub_addr, Some(client)),
             )?;
         }
-        // Send join updates to leaf set
 
-        *guard = true;
+        println!(
+            "Node #{} updated leaf set: {:?}",
+            id,
+            leaf.get_set()
+                .iter()
+                .map(|e| e.key.clone())
+                .collect::<Vec<u64>>()
+        );
 
         info!("Connected to network");
 
         Ok(())
+    }
+
+    /// Blocks execution until node is connected to network.
+    pub async fn block_until_connected(&self) -> () {
+        let _ = self.is_connected.lock().await;
     }
 }
--- src/dht/service.rs
+++ src/dht/service.rs
@@ -1,12 +1,11 @@
-use log::info;
 use tonic::{Request, Response, Status};
 
 use crate::{
     dht::node::{NodeConnection, NodeInfo},
     rpc::node::{
         node_service_client::NodeServiceClient, node_service_server::NodeService,
-        GetNodeIdResponse, JoinRequest, JoinResponse, LeafSetEntry, LeaveRequest, QueryRequest,
-        QueryResponse,
+        GetNodeIdResponse, GetNodeStateResponse, JoinRequest, JoinResponse, LeafSetEntry,
+        LeaveRequest, QueryRequest, QueryResponse, UpdateNeighborsRequest,
     },
 };
 
@@ -16,39 +15,48 @@ impl NodeService for super::node::Node {
         &self,
         _request: Request<()>,
     ) -> std::result::Result<Response<GetNodeIdResponse>, Status> {
-        {
-            let _ = self.is_connected.lock().await;
-        }
+        self.block_until_connected().await;
+
         Ok(Response::new(GetNodeIdResponse { id: self.id }))
+    }
+
+    async fn get_node_state(
+        &self,
+        _request: Request<()>,
+    ) -> std::result::Result<Response<GetNodeStateResponse>, Status> {
+        self.block_until_connected().await;
+
+        let leaf = self.leaf.read().await;
+
+        Ok(Response::new(GetNodeStateResponse {
+            id: self.id,
+            leaf_set: leaf
+                .get_set()
+                .iter()
+                .map(|e| LeafSetEntry {
+                    id: e.value.info.id.clone(),
+                    pub_addr: e.value.info.pub_addr.clone(),
+                })
+                .collect(),
+        }))
     }
 
     async fn join(
         &self,
         request: Request<JoinRequest>,
     ) -> std::result::Result<Response<JoinResponse>, Status> {
-        {
-            let _ = self.is_connected.lock().await;
-        }
+        self.block_until_connected().await;
 
         let req = request.into_inner();
-        // Check if joining node would be next neighbor (falls in between itself and next node)
-        // If found -> add to leaf set, return cloned leaf set and routing table
-
-        info!("Node #{:x} got request {:?}", self.id, req);
 
         let conn = {
             let leaf = self.leaf.read().await;
             leaf.get(req.id).clone()
         };
 
-        info!(
-            "Node #{:x} found {:?} responsible for #{:x}",
-            self.id, conn, req.id
-        );
-
         if let Some(conn) = conn {
             if conn.info.id != self.id {
-                // If not neighbor -> forward request to responsible node
+                // Forward to node in leaf set
                 return conn.client.unwrap().join(Request::new(req.clone())).await;
             }
             // Current node is previous to joining node
@@ -85,8 +93,6 @@ impl NodeService for super::node::Node {
                 },
             )?;
 
-            info!("Node #{:x} Leafset after join {:?}", self.id, leaf);
-
             return Ok(Response::new(JoinResponse {
                 id: self.id,
                 pub_addr: self.pub_addr.clone(),
@@ -109,9 +115,8 @@ impl NodeService for super::node::Node {
         &self,
         request: Request<LeaveRequest>,
     ) -> std::result::Result<Response<()>, Status> {
-        {
-            let _ = self.is_connected.lock().await;
-        }
+        self.block_until_connected().await;
+
         todo!()
     }
 
@@ -119,16 +124,80 @@ impl NodeService for super::node::Node {
         &self,
         request: Request<QueryRequest>,
     ) -> std::result::Result<Response<QueryResponse>, Status> {
-        {
-            let _ = self.is_connected.lock().await;
+        self.block_until_connected().await;
+
+        let req = request.into_inner();
+
+        let conn = {
+            let leaf = self.leaf.read().await;
+            leaf.get(req.key).clone()
+        };
+
+        if let Some(conn) = conn {
+            if conn.info.id != self.id {
+                // Forward request using leaf set
+                return conn.client.unwrap().query(Request::new(req.clone())).await;
+            }
+
+            // Node is the owner of key
+            return Ok(Response::new(QueryResponse { id: self.id }));
         }
-        // Check if key belongs to node
-        // If found -> execute query and return
 
-        // If not -> check leaf set for key owners
-        // If found -> forward request
+        // Forward request using routing table
+        let mut client = {
+            let table = self.table.read().await;
+            let info = table.route(req.key, 0)?;
+            NodeServiceClient::connect(info.pub_addr.clone())
+                .await
+                .unwrap()
+        };
+        client.query(Request::new(req.clone())).await
+    }
 
-        // If not -> forward request using routing table
-        todo!()
+    async fn update_neighbors(
+        &self,
+        request: Request<UpdateNeighborsRequest>,
+    ) -> std::result::Result<Response<()>, Status> {
+        self.block_until_connected().await;
+
+        let req = request.into_inner();
+
+        let conn = {
+            let leaf = self.leaf.read().await;
+            leaf.get(req.id).clone()
+        };
+
+        if let Some(conn) = conn {
+            if conn.info.id != req.id {
+                let mut leaf = self.leaf.write().await;
+
+                // Insert as neighbor
+                leaf.insert(
+                    req.id,
+                    NodeConnection {
+                        info: NodeInfo {
+                            id: req.id,
+                            pub_addr: req.pub_addr.clone(),
+                        },
+                        client: Some(
+                            NodeServiceClient::connect(req.pub_addr.clone())
+                                .await
+                                .unwrap(),
+                        ),
+                    },
+                )?;
+
+                println!(
+                    "Node #{} updated leaf set: {:?}",
+                    self.id,
+                    leaf.get_set()
+                        .iter()
+                        .map(|e| e.key.clone())
+                        .collect::<Vec<u64>>()
+                );
+            }
+        }
+
+        Ok(Response::new(()))
     }
 }
