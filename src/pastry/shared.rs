848f90a52e2fc1cfe379679d194d234eeaeded08
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-10-10 15:39:41 -0300

    add get_value method

--- src/pastry/shared.rs
+++ src/pastry/shared.rs
@@ -8,7 +8,12 @@ impl<T, U> KeyValuePair<T, U> {
     pub fn new(key: T, value: U) -> Self {
         Self { key, value }
     }
+
     pub fn get_key(&self) -> &T {
         &self.key
+    }
+
+    pub fn get_value(&self) -> &U {
+        &self.value
     }
 }
