08bf34c88bcd385627cf95666567db53fa2e3086
Alvaro Tedeschi <alvarotedeschi@gmail.com>
2023-11-12 15:10:26 -0300

    improve hasher

--- src/internal/dht/store.rs
+++ src/internal/dht/store.rs
@@ -1,8 +1,26 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    hash::{Hash, Hasher},
+};
+
+#[derive(Debug, PartialEq, Eq)]
+struct PreHashedKey(u64);
+
+impl Hash for PreHashedKey {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_u64(self.0);
+    }
+}
+
+impl From<u64> for PreHashedKey {
+    fn from(value: u64) -> Self {
+        PreHashedKey(value)
+    }
+}
 
 #[derive(Debug)]
 pub struct Store {
-    store: HashMap<u64, Vec<u8>>,
+    store: HashMap<PreHashedKey, Vec<u8>>,
 }
 
 impl Store {
@@ -13,25 +31,29 @@ impl Store {
     }
 
     pub fn get(&self, key: &u64) -> Option<&Vec<u8>> {
-        self.store.get(key)
+        self.store.get(&key.clone().into())
     }
 
     pub fn set(&mut self, key: &u64, value: &[u8]) -> Option<Vec<u8>> {
-        self.store.insert(*key, value.to_vec())
+        self.store.insert(key.clone().into(), value.to_vec())
     }
 
     pub fn delete(&mut self, key: &u64) -> Option<Vec<u8>> {
-        self.store.remove(key)
+        self.store.remove(&key.clone().into())
     }
 
-    pub fn list(&self) -> Vec<(&u64, &Vec<u8>)> {
-        self.store.iter().collect()
+    pub fn list(&self) -> Vec<(u64, &Vec<u8>)> {
+        self.store.iter().map(|e| (e.0 .0, e.1)).collect()
     }
 
-    pub async fn get_entries<F>(&self, f: F) -> Vec<(&u64, &Vec<u8>)>
+    pub async fn get_entries<F>(&self, f: F) -> Vec<(u64, &Vec<u8>)>
     where
         F: Fn(u64) -> bool,
     {
-        self.store.iter().filter(|(&ref key, _)| f(*key)).collect()
+        self.store
+            .iter()
+            .filter(|(&ref key, _)| f(key.0))
+            .map(|e| (e.0 .0, e.1))
+            .collect()
     }
 }
